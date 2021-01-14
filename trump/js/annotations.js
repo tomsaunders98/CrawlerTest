50500ba9a9b74ebc03adfe390f0b39e394891b21
Tom Saunders <tomjs@protonmail.com>
2021-01-14 18:50:05 +0000

    Adding Trump Tweets

---
+++ trump/js/annotations.js
@@ -0,0 +1,471 @@
+var annotations = [
+  {
+    type:d3.annotationCalloutElbow,
+    note: {
+        label: "Trump launches presidential campaign"
+    },
+    data:{
+        x: "2015-06-16 15:57:23",
+        y: 10619
+
+    },
+    color: ["black"],
+    dx:50,
+    dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Size",
+      label: "The size of the circle indicates the number of retweets"
+  },
+  data:{
+      x: "2015-07-04 03:57:30",
+      y: 322
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Colour",
+      label: "The colour indicates whether the tweet is a retweet (green), has been deleted (grey) or has been flagged (red).",
+      wrap: 300
+  },
+  data:{
+      x: "2016-01-03 04:06:35",
+      y: 0
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Muslim Immigration",
+      label: "Donald Trump calls for Muslim ban",
+  },
+  data:{
+      x: "2015-12-07 22:32:07",
+      y: 6158
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Muslim Immigration",
+      label: "Donald Trump calls for Muslim ban",
+  },
+  data:{
+      x: "2015-12-07 22:32:07",
+      y: 6158
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Voter Fraud",
+      label: "After losing the Iowa Caucus, Trump alleges that Cruz 'stole it'.",
+  },
+  data:{
+      x: "2016-02-03 14:07:25",
+      y: 6557
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "New Hampshire",
+      label: "Trump wins the New Hampshire primary.",
+  },
+  data:{
+      x: "2016-02-09 04:06:58",
+      y: 6849
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Indiana",
+      label: "Trump wins the Indiana priamry, becoming the presumptive Republican nominee.",
+  },
+  data:{
+      x: "2016-05-03 23:38:34",
+      y: 32088
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Mike Pence",
+      label: "Trump picks Mike Pence as his running mate.",
+  },
+  data:{
+      x: "2016-07-15 14:50:55",
+      y: 79700
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Trump wins",
+      label: "Trump wins the 2016 election.",
+  },
+  data:{
+      x: "2016-11-08 11:43:14",
+      y: 498035
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Womens March",
+      label: "The day after Trump is inaugurated four million attend the Women's March.",
+  },
+  data:{
+      x: "2017-01-22 12:47:21",
+      y: 182320
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Voter Fraud",
+      label: "Trump claims that 3–5 million illegal votes cost him the popular vote.",
+  },
+  data:{
+      x: "2017-01-25 12:10:01",
+      y: 110259
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Trump Tower",
+      label: "Trump accuses Obama of intercepting communications at Trump Tower during the campaign.",
+      wrap: 300
+  },
+  data:{
+      x: "2017-03-04 11:35:20",
+      y: 140761
+
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Comey",
+      label: "Trump fires FBI director James Comey.",
+  },
+  data:{
+      x: "2017-05-10 02:42:52",
+      y: 78669
+
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Paris",
+      label: "Trump withdraws from the Paris Climate Accord.",
+  },
+  data:{
+      x: "2017-06-01 01:05:17",
+      y: 66560
+
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Charlottesville",
+      label: "Trump condemns violence 'on all sidees' at Charlottesvile rally.",
+  },
+  data:{
+      x: "2017-08-12 17:19:13",
+      y: 164760
+
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Tax",
+      label: "The Senate passes Trump's tax cuts.",
+  },
+  data:{
+      x: "2017-12-20 06:09:06",
+      y: 118874
+
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Nuclear",
+      label: "Trump says his nuclear button is larger than that of Kim Jong-un .",
+  },
+  data:{
+      x: "2018-01-03 00:49:19",
+      y: 430222
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Genius",
+      label: "Trump declares himself 'a very stable genius'.",
+  },
+  data:{
+      x: "2018-01-06 12:30:51",
+      y: 119593
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Brett",
+      label: "Brett Kavanaugh appointed to the US Supreme Court.",
+  },
+  data:{
+      x: "2018-10-05 14:59:41",
+      y: 171630
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Treason",
+      label: "Trump retweets image calling for Rosenstein and Muller to be imprisoned for treason.",
+  },
+  data:{
+      x: "2018-11-28 01:40:18",
+      y: 0
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Mueller",
+      label: "Mueller report is released.",
+  },
+  data:{
+      x: "2019-04-18 12:54:47",
+      y: 124939
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Go back",
+      label: "Trump attacks progressive democrats.",
+  },
+  data:{
+      x: "2019-07-14 12:27:52",
+      y: 165089
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Impeachment (1)",
+      label: "Nancy Pelosi launches formal impeachment inquiry.",
+  },
+  data:{
+      x: "2019-09-24 18:12:09",
+      y: 123007
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Soleimani",
+      label: "Trump announces death of Iranian Major General Qasem Soleimani.",
+  },
+  data:{
+      x: "2020-01-03 12:44:30",
+      y: 303007
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "COVID-19",
+      label: "First victim of COVID-19 reported.",
+  },
+  data:{
+      x: "2020-02-29 17:02:35",
+      y: 109825
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Impeachment (1)",
+      label: "Trump acquited by the Senate.",
+  },
+  data:{
+      x: "2020-02-05 22:07:04",
+      y: 273366
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "2020",
+      label: "Trump accepts the Republican nomination for the 2020 election.",
+  },
+  data:{
+      x: "2020-08-24 15:25:48",
+      y: 114709
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "BLM",
+      label: "Trump responds to the Black Lives Matter protests.",
+  },
+  data:{
+      x: "2020-05-29 04:53:14",
+      y: 0
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "2020 (1)",
+      label: "Joe Biden wins the 2020 US Election.",
+  },
+  data:{
+      x: "2020-11-07 15:36:36",
+      y: 1188311
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Raffensperger",
+      label: "In leaked phone call Trump demands Brad Raffensperger, Georgia Secretary of State, 'find 11,780 votes'.",
+  },
+  data:{
+      x: "2021-01-03 13:57:37",
+      y: 198671
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Mob",
+      label: "Trump mob overruns capital, Trump refuses to concede.",
+  },
+  data:{
+      x: "2021-01-06 19:38:58",
+      y: 582183
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+},
+{
+  type:d3.annotationCalloutElbow,
+  note: {
+      title: "Banned",
+      label: "Trump is banned from Twitter.",
+  },
+  data:{
+      x: "2021-01-08 15:44:28",
+      y: 510761
+  },
+  color: ["black"],
+  dx:50,
+  dy:-50
+}
+]
---
+++ trump/js/main.js
@@ -0,0 +1,294 @@
+// Bind leftward scroll to mousewheel
+$(document).ready(function() {
+  $('.box').mousewheel(function(e, delta) {
+    if (Math.abs(e.deltaX)) {
+      return
+    } else {
+      this.scrollLeft += (e.deltaY * 15);
+    }
+    e.preventDefault();
+  });
+});
+
+function BuildLine() {
+  // data range
+  var launchcamp = new Date(2015, 5, 16);
+  var fday = new Date(2021, 1, 20);
+
+  //Load Tweets
+  d3.csv("data/tweets.csv",
+    function(d) {
+      d.date = d3.timeParse("%Y-%m-%d %H:%M:%S")(d.date);
+      d.id = d.id;
+      d.text = d.text;
+      d.rt = (d.isRetweet === "f") ? false : true;
+      d.delete = (d.isDeleted === "f") ? false : true;
+      d.flag = (d.isFlagged === "f") ? false : true;
+      d.type = (d.flag === true) ? "f" : (d.delete === true) ? "d" : (d.rt === true) ? "rt" : false;
+      d.fav = parseInt(d.favorites);
+      d.rtno = parseInt(d.retweets);
+      d.device = d.device;
+      return d;
+    },
+    function(data) { //filter by date range
+      data = data.filter(function(d) {
+        return d.date >= launchcamp & d.date < fday;
+      })
+      console.log(data)
+      // Add number of tweets
+      d3.select(".dl")
+        .html(data.length)
+
+      // Calculate ticks + svg length
+      diff = d3.timeDay.count(launchcamp, fday);
+      bwidth = (window.innerWidth / 20) * diff;
+      width = window.innerWidth;
+      wheight = window.innerHeight;
+      // build svg
+      var svg = d3.select('.box').append("svg")
+        .style("width", bwidth + "px")
+        .style("height", 0.95 * wheight + "px")
+        .attr('viewBox', '0 0 ' + bwidth + ' ' + wheight)
+        .attr('preserveAspectRatio', 'none')
+        .attr("class", "svg")
+      // add margins
+      var margin = {
+        top: 20,
+        right: 30,
+        bottom: 30,
+        left: 100
+      }
+      g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      width = bwidth - margin.left - margin.right,
+        height = wheight - margin.top - margin.bottom;
+
+      // Scales
+      var x = d3.scaleTime()
+        .domain([new Date(2015, 5, 15), fday])
+        .range([0, width])
+        .clamp(true);
+      var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d) {
+          return d.fav;
+        }) * 1.3])
+        .range([height, 0]);
+      var z = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d) {
+          return d.rtno;
+        })])
+        .range([4, 40]);
+      var myColor = d3.scaleOrdinal()
+        .domain(["f","d", false, "rt"])
+        .range(["#e22847","#BDC3C7", "#55acee", "#17bf63"]);
+
+      //Axis
+      g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x)
+          .ticks(diff)
+          .tickFormat(d3.timeFormat("%Y-%m-%d")));
+
+      g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y));
+
+      g.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axis-title")
+        .style("text-anchor", "middle")
+        .text("Total Favourites");
+
+      //Build disappearing title box
+      d3.select(".box")
+        .on("scroll", function() {
+          box = document.getElementById("test");
+          scroll = box.scrollLeft;
+          wwidth = window.innerWidth;
+          opacity = 1 - (scroll * (1 / wwidth));
+          title = d3.select(".titlewrapper");
+          end = d3.select(".endwrapper");
+          if (opacity >= 0) {
+            title
+              .style("opacity", opacity);
+          }
+
+          if (scroll > wwidth) {
+            title
+              .style("display", "none");
+          }
+          if (scroll < wwidth) {
+            title
+              .style("display", "initial");
+          }
+          if (scroll > bwidth - 2*wwidth){
+            end
+              .style("display", "initial");
+            opacity =  -1*((scroll - (bwidth-2*wwidth))/(-wwidth)); //min-max normilization for opacity scale
+            if(opacity >= 0){
+              end
+              .style("opacity", opacity);
+            }
+          }
+          if (scroll < bwidth - 2*wwidth){
+            end
+            .style("display", "none");
+          }
+
+
+
+        });
+
+      //to wrangle tweet text for tooltip
+      function textToTweet(tweet, rt) {
+        if (rt === true) {
+          //replace "RT" if retweet
+          regex = /^RT /g,
+            tweet = tweet.replace(regex, '');
+
+          // seperate headline from rest of tweet
+          regex = /^@(\w{1,15}):(.*)/i,
+            matches = tweet.match(regex);
+          if (matches != null) {
+            headline = '<span class="abovetext"><img src="data/rtgrey.png" class="logo" />Donald Trump Retweeted </span><div class="subtweet"><span class="subtitle">@' + matches[1] + '</span><br />';
+            tweet = matches[2];
+          }
+        }
+        //Remove exra speech markst
+        regex = /(["])(?=\1)/g;
+        tweet = tweet.replace(regex, '');
+
+        regex = /^"|"$/g
+        tweet = tweet.replace(regex, '');
+
+        //convert links
+        regex = /(https?:\/\/[^\s]+)/g,
+          replace = '<a href="$1">$1</a>';
+        tweet = tweet.replace(regex, replace);
+
+        //Replace Tags
+        regex = /(^|[^@\w])@(\w{1,15})\b/g,
+          replace = '$1<a href="http://twitter.com/$2">@$2</a>';
+        tweet = tweet.replace(regex, replace);
+
+        // hashtags
+        regex = /(?:^|\B)#(?![0-9_]+\b)([a-zA-Z0-9_]{1,30})(?:\b|\r)/g,
+          replace = '<a href="https://twitter.com/hashtag/$1">#$1</a>';
+        tweet = tweet.replace(regex, replace);
+
+        //Add headline if rt
+        if (rt === true) {
+          tweet = headline + tweet + "</div>";
+        }
+        return tweet;
+      }
+
+      //vals for tooltip
+      tooltip = d3.select(".tooltip");
+      tooltext = d3.select(".text");
+      favs = d3.select(".fav");
+      rts = d3.select(".rts");
+      hour = d3.select(".hour");
+      day = d3.select(".day");
+      device = d3.select(".device");
+      link = d3.select(".link");
+
+      //Tooltip
+      var showTooltip = function(d) {
+
+        //Make Circle Brighter
+        d3.select(this)
+          .style("opacity", 0.7)
+        //Find point to get top/left values
+        rect = d3.select(this).node().getBoundingClientRect();
+        // convert dates to twitter style
+        days = d.date.toLocaleDateString(navigator.language, {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'short',
+          day: '2-digit'
+        });
+        hours = d.date.toLocaleTimeString(navigator.language, {
+          hour: '2-digit',
+          minute: '2-digit'
+        });
+        // enter data
+        tooltext
+          .html(textToTweet(d.text, d.rt))
+        favs
+          .html(d.fav)
+        rts
+          .html(d.rtno)
+        hour
+          .html(hours)
+        day
+          .html(days)
+        link
+          .attr("href", "https://twitter.com/realdonaltrump/status/" + d.id)
+        device
+          .html(d.device)
+        tooltip
+          .transition()
+          .duration(200)
+        tooltip
+          .style("opacity", 1)
+          .style("left", rect.left + rect.width + 30 + "px")
+          .style("top", rect.top - 150 + "px")
+      }
+
+      //Hide tooltip (w/ slow transition)
+      var hidetooltip = function(d) {
+        d3.select(this)
+          .style("opacity", 1)
+
+        tooltip
+          .transition()
+          .duration(3000)
+          .style("opacity", 0)
+
+      }
+      //Annotations
+      var pt = d3.timeParse("%Y-%m-%d %H:%M:%S");
+      var makeAnnotations = d3.annotation()
+        .accessors({
+          x: function(d) {
+            return x(pt(d.x));
+          },
+          y: function(d) {
+            return y(parseInt(d.y)) ;
+          }
+        })
+        .annotations(annotations)
+
+      d3.select("svg")
+        .append("g")
+        .attr("class", "annotation-group")
+        .call(makeAnnotations)
+
+      //add dots
+      svg.append('g')
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("class", "bubbles")
+        .attr("cx", function(d) {
+          return x(d.date);
+        })
+        .attr("cy", function(d) {
+          return y(d.fav);
+        })
+        .attr("r", function(d) {
+          return z(d.rtno);
+        })
+        .style("fill", function(d) {
+          return myColor(d.type);
+        })
+        .on("mouseover", showTooltip)
+        .on("mouseout", hidetooltip)
+    })
+}
